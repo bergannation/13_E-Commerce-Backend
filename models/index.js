9d7fad513a25c19936a37ae01b9cf4600ca76b37
Travis Brown <bergannation@gmail.com>
2021-06-16 16:14:38 -0500

    updated models

--- models/index.js
+++ models/index.js
@@ -1,16 +1,29 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
-// Categories have many Products
+// Associations - hasMany
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Associations - belongsToMany
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Associations - belongsToMany
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
